6216470bfad37ae2cf97ab8ba4c3848da396d8ce
develop <ikhlas.firlana@gmail.com>
2019-02-06 16:55:37 +0700

    nice animation

--- animate/src/app/app.module.ts
+++ animate/src/app/app.module.ts
@@ -2,12 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 
-import {MatListModule} from '@angular/material/list';
-import {MatIconModule} from '@angular/material/icon';
+import { MatFormFieldModule, MatInputModule, MatButtonModule, MatListModule, MatIconModule } from '@angular/material';
+
+// import {MatFormFieldModule} from '@angular/material/form-field';
+// import {MatButtonModule} from '@angular/material/button';
+// import {MatInputModule} from '@angular/material/input';
+// import {MatListModule} from '@angular/material/list';
+// import {MatIconModule} from '@angular/material/icon';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormComponent } from './form/form.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -17,9 +23,13 @@ import { FormComponent } from './form/form.component';
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
+    FormsModule,
     AppRoutingModule,
+    MatFormFieldModule,
+    MatInputModule,
     MatListModule,
     MatIconModule,
+    MatButtonModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- animate/src/app/form/form.component.ts
+++ animate/src/app/form/form.component.ts
@@ -1,19 +1,114 @@
 import { Component, OnInit } from '@angular/core';
+import { trigger, state, style, transition, animate } from '@angular/animations';
 
 @Component({
   selector: 'app-form',
   templateUrl: './form.component.html',
-  styleUrls: ['./form.component.scss']
+  styleUrls: ['./form.component.scss'],
+  animations: [
+    trigger('matItemState', [
+      //
+      state('normal', style({
+        'background-color': 'red',
+        // transform: 'translateY(0)',
+      })),
+      state('highlighted', style({
+        'background-color': 'blue',
+        // transform: 'translateY(100px)',
+      })),
+      transition('normal <=> highlighted', animate(8000))
+    ]),
+    trigger('wildItemState', [
+      //
+      state('normal', style({
+        'background-color': 'red',
+        width: '80px',
+        height: '80px',
+        // transform: 'translateY(0)',
+      })),
+      state('highlighted', style({
+        'background-color': 'blue',
+        width: '80px',
+        height: '80px',
+        // transform: 'translateY(100px)',
+      })),
+      state('shrunken', style({
+        'background-color': 'green',
+        width: '40px',
+        height: '40px',
+        // transform: 'translateY(100px)',
+      })),
+      transition('normal => highlighted', animate(300)),
+      transition('highlighted => normal', animate(800)),
+      transition('shrunken <=> *', [
+        style({
+          'background-color': 'yellow',
+        }),
+        animate(400, style({
+          'background-color': 'purple',
+          width: '40px',
+          height: '40px',
+        })),
+        animate(600),
+        // animate(4000, style({
+        //   'background-color': 'yellow',
+        // })),
+        // animate(6000, style({
+        //   'background-color': 'purple',
+        // })),
+      ]),
+    ]),
+    trigger('list', [
+      //
+      state('in', style({
+        opacity: 1,
+        transform: 'translateX(0)',
+      })),
+      transition('void => *', [
+        style({
+          opacity: 0,
+          transform: 'translateX(-100px)',
+        }),
+        animate(600)
+      ]),
+      transition('* => void', [
+        animate(600, style({
+          transform: 'translateX(100px)',
+          opacity: 0,
+        }))
+      ])
+    ]),
+  ],
 })
 export class FormComponent implements OnInit {
-  data: any[] = [];
+  state = 'normal';
+  wildState = 'normal';
+  data: string[] = [
+    'HELLO This Dummy Data',
+    'Again dummy data',
+  ];
+  item: string;
 
   constructor() { }
 
   ngOnInit() {
   }
 
-  onAdd() {
+  private onAdd(value: string): void {
     console.log('run add');
+    this.data.push(value);
+    this.data.splice(0, 1);
+    this.item = '';
+  }
+
+  onAddAnimate_one(value: string): void {
+   this.onAdd(value);
+   this.state === 'normal' ? this.state = 'highlighted' : this.state = 'normal';
+   this.wildState === 'normal' ? this.wildState = 'highlighted' : this.wildState = 'normal';
+  }
+
+  onAddAnimate_two(value: string): void {
+    this.onAdd(value);
+    this.wildState = 'shrunken';
   }
 }
